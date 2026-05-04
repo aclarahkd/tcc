4bc209ad06ea11a9b74f833dd4499697c1c79ea4
aclarahkd <aclararigobeli@gmail.com>
2026-05-04 08:41:39 -0300

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:projetotcc/main.dart';
+import 'package:projetotcc/tela1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
